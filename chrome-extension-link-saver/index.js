d16076c3aebfde31ac5eb1240491c899fa18bc0d
nikhilnrr <54225313+nikhilnrr@users.noreply.github.com>
2023-02-22 23:59:03 +0530

    Add files via upload

--- chrome-extension-link-saver/index.js
+++ chrome-extension-link-saver/index.js
@@ -1,18 +1,50 @@
-let myLeads = ["lado", "puti", "muji"]
+let myLeads = []
 
 
 let inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 
 const ulEl = document.getElementById("ul-El")
-console.log(ulEl)
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("save-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
 
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputBtn.value)
-    console.log("gsfseffes")
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        renderLeads(myLeads)
+    })
 })
 
-for(let i = 0 ; i<myLeads.length; i++){
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>" 
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = "" //input box khali garcha
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
 }
